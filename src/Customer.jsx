0f12e277aaa8a847755d46b9fe7f0c423037dedd
Keerthi Vasan <rpkeerthivasan@gmail.com>
2025-05-07 12:21:52 +0530

    after

--- src/Customer.jsx
+++ src/Customer.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Customernav from './components/Customernav';
 import Customerhome from './components/Customerhome';
-import TrainingList from './components/Traininglist';
 import Checkbox from './components/Checkbox';
 import Custproduct from './components/Custproduct';
 import ReviewPage from './components/Reviewpage';
 import Login from './components/Login';
+import Traininglist from './components/Traininglist';
  
 const Customer = () => {
   return (
@@ -16,7 +16,7 @@ const Customer = () => {
       <Routes>
         <Route path="home" element={<Customerhome />} />
         <Route path="/" element={<Customerhome />} />
-        <Route path="/" element={<TrainingList />} />
+        <Route path="/" element={<Traininglist/>} />
         <Route path="checkbox" element={<Checkbox />} />
         <Route path="products" element={<Custproduct />} />
         <Route path="review" element={<ReviewPage />} />
